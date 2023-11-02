e2bfb20abe283f6da1871de3844cda2e9fbbc256
Manuel <97695681+manudelu@users.noreply.github.com>
2023-11-02 11:27:51 +0000

    Add files via upload

---
+++ Assignment1_EmbeddedSystems.X/main.c
@@ -0,0 +1,121 @@
+// DSPIC30F4011 Configuration Bit Settings
+// 'C' source line config statements
+// FOSC
+#pragma config FPR = XT            // Primary Oscillator Mode (XT)
+#pragma config FOS = PRI           // Oscillator Source (Primary Oscillator)
+#pragma config FCKSMEN = CSW_FSCM_OFF// Clock Switching and Monitor (Sw Disabled, Mon Disabled)
+
+// FWDT
+#pragma config FWPSB = WDTPSB_16   // WDT Prescaler B (1:16)
+#pragma config FWPSA = WDTPSA_512  // WDT Prescaler A (1:512)
+#pragma config WDT = WDT_OFF       // Watchdog Timer (Disabled)
+
+// FBORPOR
+#pragma config FPWRT = PWRT_64  // POR Timer Value (64ms)
+#pragma config BODENV = BORV20  // Brown Out Voltage (Reserved)
+#pragma config BOREN = PBOR_ON  // PBOR Enable (Enabled)
+#pragma config LPOL = PWMxL_ACT_HI// Low-side PWM Output Polarity (Active High)
+#pragma config HPOL = PWMxH_ACT_HI// High-side PWM Output Polarity (Active High)
+#pragma config PWMPIN = RST_IOPIN// PWM Output Pin Reset (Control with PORT/TRIS regs)
+#pragma config MCLRE = MCLR_EN  // Master Clear Enable (Enabled)
+
+// FGS
+#pragma config GWRP = GWRP_OFF      // General Code Segment Write Protect (Disabled)
+#pragma config GCP = CODE_PROT_OFF  // General Segment Code Protection (Disabled)
+
+// FICD
+#pragma config ICS = ICS_PGD       // Comm Channel Select (Use PGC/EMUC and PGD/EMUD)
+
+#include "xc.h"
+
+#define FOSC 7372800.0 //Hz Clock breadboard
+#define REG_SXT_BIT 65535.0 // MAX 16 bit register
+#define TIMER1 1
+#define TIMER2 2
+#define LED1 1
+void tmr_setup_period(int timer, int ms);
+void tmr_wait_period(int timer);
+
+void tmr_setup_period(int timer, int ms){ // Set the prescaler and the PR value
+// Fosc = 737280 Hz -> Fcy = Fosc / 4 = 184320 number of clocks in one second so in 0.1 secon there would be 184320 clocks steps
+// this is too high to be put in a 16 bit register (max 65535)
+// If we set a prescaler of 1:8 we have 184320/8 = 23040 clock steps
+    double Fcy = FOSC / 4.0;
+    double clock_steps = Fcy * (ms/1000.0);
+    int count = 0;
+    if (clock_steps > REG_SXT_BIT){
+        double prescaler_value[3] = {8.0,8.0,4.0};
+        while(clock_steps > REG_SXT_BIT){
+            clock_steps = clock_steps/prescaler_value[count];
+            count += 1;           
+        }
+    }
+    if (timer == TIMER1){
+        TMR1 = 0.0; // Reset the timer
+        PR1 = clock_steps; // Set the desired count value
+        T1CONbits.TCKPS = count; // set PRESCALER
+    }
+    if (timer == TIMER2){
+        TMR2 = 0.0;
+        PR2 = clock_steps; 
+        T2CONbits.TCKPS = count; // set PRESCALER
+    }
+}
+
+void tmr_wait_period (int timer) {
+    if(timer == TIMER1){
+        while(IFS0bits.T1IF == 0){
+            // Wait until the flag is high -> The timer finished to count
+        }
+    }
+    
+    if(timer == TIMER2){
+       while(IFS0bits.T2IF == 0){
+            // Wait until the flag is high -> The timer finished to count
+        } 
+    }
+        
+}
+
+void tmr_wait_ms(int timer, int ms){
+    tmr_setup_period(timer, ms);
+    if (timer == TIMER1){
+        IFS0bits.T1IF = 0; // Reset the flag
+        T1CONbits.TON = 1; // Starts the timer
+    }
+    if (timer == TIMER2){ // CONTROLLARE T2
+        IFS0bits.T2IF = 0; // Reset the flag
+        T2CONbits.TON = 1; // Starts the timer
+    }
+    tmr_wait_period(timer);
+}
+
+char algorithm() {
+    tmr_wait_ms(TIMER2, 7);
+    U1BRG = 11; // (7372800 / 4) / (16 * 9600) ? 1
+    U1MODEbits.UARTEN = 1; // enable UART
+    U1STAbits.UTXEN = 1;   // enable U1TX (must be after UARTEN)
+    U1TXREG = 'C'; // send ?C?
+}
+
+int main() {
+    tmr_setup_period(TIMER1, 10);
+    
+    SPI1CONbits.MSTEN = 1; // master mode
+    SPI1CONbits.MODE16 = 0; // 8?bit mode
+    SPI1CONbits.PPRE = 3; // 1:1 primary prescaler
+    SPI1CONbits.SPRE = 3; // 5:1 secondary prescaler
+    SPI1STATbits.SPIEN = 1; // enable SPI
+    
+    tmr_setup_period(TIMER1,1000);
+    
+    while(1) {
+        algorithm();
+        tmr_wait_period(TIMER1);
+        //SPI1BUF = c;
+        // code to handle the assignment   
+    }
+        
+    return 0;
+}
+
