7520e9f7baea34385881bd01924494b530663d75
Manuel <97695681+manudelu@users.noreply.github.com>
2023-11-14 10:18:04 +0100

    Update main.c

--- Assignment1.X/main.c
+++ Assignment1.X/main.c
@@ -8,17 +8,17 @@
 // DSPIC30F4011 Configuration Bit Settings
 // 'C' source line config statements
 
-// FOSC
+// FOSC Configuration
 #pragma config FPR = XT                 // Primary Oscillator Mode (XT)
 #pragma config FOS = PRI                // Oscillator Source (Primary Oscillator)
 #pragma config FCKSMEN = CSW_FSCM_OFF   // Clock Switching and Monitor (Sw Disabled, Mon Disabled)
 
-// FWDT
+// FWDT Configuration
 #pragma config FWPSB = WDTPSB_16        // WDT Prescaler B (1:16)
 #pragma config FWPSA = WDTPSA_512       // WDT Prescaler A (1:512)
 #pragma config WDT = WDT_OFF            // Watchdog Timer (Disabled)
 
-// FBORPOR
+// FBORPOR Configuration
 #pragma config FPWRT = PWRT_64          // POR Timer Value (64ms)
 #pragma config BODENV = BORV20          // Brown Out Voltage (Reserved)
 #pragma config BOREN = PBOR_ON          // PBOR Enable (Enabled)
@@ -27,16 +27,14 @@
 #pragma config PWMPIN = RST_IOPIN       // PWM Output Pin Reset (Control with PORT/TRIS regs)
 #pragma config MCLRE = MCLR_EN          // Master Clear Enable (Enabled)
 
-// FGS
+// FGS Configuration
 #pragma config GWRP = GWRP_OFF          // General Code Segment Write Protect (Disabled)
 #pragma config GCP = CODE_PROT_OFF      // General Segment Code Protection (Disabled)
 
-// FICD
+// FICD Configuration
 #pragma config ICS = ICS_PGD            // Comm Channel Select (Use PGC/EMUC and PGD/EMUD)
 
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
+// Include necessary libraries and headers
 #include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,35 +44,40 @@
 volatile CircularBuffer cb;
 volatile char charCount[4];
 
+// Interrupt handler for UART receive
 void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt() {
     IFS1bits.U2RXIF = 0;
     char receivedChar = U2RXREG; // Copy char from received REG
-    cb_push(&cb, receivedChar); // When a new char is received push it
+    cb_push(&cb, receivedChar);  // When a new char is received, push it to the circular buffer
 }
 
+
+// Interrupt handler for external interrupt 0 (INT0)
 void __attribute__ ((__interrupt__ , __auto_psv__ ) ) _INT0Interrupt() {
     IFS0bits.INT0IF = 0;
-    IEC0bits.INT0IE = 0; // disable interrupt for INT0
-    sprintf(charCount, "%d", cb.count);
-    uart_write(charCount);
-    tmr_setup_period(TIMER3, 100); // start timer 3  //////////VEDI//////////
-    IEC0bits.T3IE = 1; // enable interrupt for T3
+    IEC0bits.INT0IE = 0;                 // Disable interrupt for INT0
+    sprintf(charCount, "%d", cb.count);  // Convert count to string
+    uart_write(charCount);               // Send count via UART
+    tmr_setup_period(TIMER3, 100);       // Start timer 3  //////////VEDI//////////
+    IEC0bits.T3IE = 1;                   // Enable interrupt for timer 3
 }
 
+// Interrupt handler for Timer 3
 void __attribute__ (( __interrupt__ , __auto_psv__ ) ) _T3Interrupt() {
     IFS0bits.T3IF = 0;
-    IFS0bits.INT0IF = 0; // reset INT0 IF
-    T3CONbits.TON = 0; // stop timer 3
+    IFS0bits.INT0IF = 0;    // Reset INT0 IF
+    T3CONbits.TON = 0;      // Stop timer 3
     TMR3 = 0;
     IEC0bits.T3IE = 0;
-    IEC0bits.INT0IE = 1; // enable interrupt for INT0
+    IEC0bits.INT0IE = 1;    // Enable interrupt for INT0
 }
 
+// Main function
 int main() {
-    // Initialize the LCD and UART
+    // Initialize the LCD, UART and timers
     spi_setup();
     uart_setup();
-    tmr_wait_ms(TIMER1, 1000);  // Wait 1s to start the SPI correctly
+    tmr_wait_ms(TIMER1, 1000);    // Wait 1s to start the SPI correctly
     tmr_setup_period(TIMER2, 10); // Period definition
     
     // Initialize Circular Buffer Variables
@@ -90,28 +93,21 @@ int main() {
     // Buffer to hold the "Char Recv: XXX" string
     char charCountStr[16]= "Char Recv:";
     
+    // Display the initial message on the LCD
     for (int i = 0; charCountStr[i] != '\0'; i++) {
         lcd_write(i + 16, charCountStr[i]);
     }
     
     lcd_move_cursor(0);
     
-    IEC0bits.INT0IE = 1; // enable interrupt for INT0
-    
-//    TRISBbits.TRISB0 = 0; // Set the LED D3 as OUT
-//    TRISBbits.TRISB1 = 0; // Set the LED D4 as OUT
+    IEC0bits.INT0IE = 1;    // Enable interrupt for INT0
 
     while (1) {  
         // Delay for 7ms to simulate the algorithm execution time
         algorithm();
         
-        // Check Overflow
-        /*if (U2STAbits.OERR == 1) { // Was it pressed before?
-            LATBbits.LATB0 = 1;
-        }*/
-        
         IEC1bits.U2RXIE = 0;
-        int read = cb_pop(&cb, &readChar); //READ DATA FROM BUFFER
+        int read = cb_pop(&cb, &readChar); // Read data from buffer
         IEC1bits.U2RXIE = 1;
                 
         if (read == 1) {
@@ -119,7 +115,7 @@ int main() {
             lcd_write(writeIndex, readChar); 
             writeIndex++;
             
-            writeIndex = writeIndex % 16; // when it reaches the end go back to the start
+            writeIndex = writeIndex % 16; // When it reaches the end, go back to the start
             
             // Clear the first row of the LCD            
             if (readChar == '\r' || readChar == '\n' || writeIndex == 0) {
@@ -133,15 +129,16 @@ int main() {
                 lcd_write(i+16, charCountStr[i]);
         }
         
+        // Reset count and clear LCD on external interrupt 1 (INT1) trigger
         if(IFS1bits.INT1IF == 1){
-                IFS1bits.INT1IF = 0;
-                cb.count = 0;
-                writeIndex = 0;
-                lcd_clear(0, 32);
-                sprintf(charCountStr, "Char Recv: %d", cb.count);
-                for (int i=0; charCountStr[i] != '\0'; i++) 
-                    lcd_write(i+16, charCountStr[i]);
-            }
+            IFS1bits.INT1IF = 0;
+            cb.count = 0;
+            writeIndex = 0;
+            lcd_clear(0, 32);
+            sprintf(charCountStr, "Char Recv: %d", cb.count);
+            for (int i=0; charCountStr[i] != '\0'; i++) 
+                lcd_write(i+16, charCountStr[i]);
+        }
         
         lcd_move_cursor(writeIndex); // Set cursor at the desired position
         tmr_wait_period(2); // Wait before next loop
